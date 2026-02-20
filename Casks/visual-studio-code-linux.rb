cc2e7c783bfcd244ac9efcd33d4b3180f77d171e
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-02-20 04:52:52 +0000

    visual-studio-code-linux 1.109.5

--- Casks/visual-studio-code-linux.rb
+++ Casks/visual-studio-code-linux.rb
@@ -2,9 +2,9 @@ cask "visual-studio-code-linux" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-  version "1.109.0"
-  sha256 arm64_linux:  "012ce913bdb5441022545c6718244c0968ff4456be4e9c40e20699245c64ec8d",
-         x86_64_linux: "37c7415d8a416daca6b865856fcfbbeffe3227a31ea397aaa18a6796c8c694df"
+  version "1.109.5"
+  sha256 arm64_linux:  "9e772a3469ce0ce82eeba8e9ee893fb296e52d2974899d653c9aab9120e77080",
+         x86_64_linux: "70160103c0dda50f651a37ee27a65a9a64a6938734cf1a0bcd49d83612bdb1a7"
 
   url "https://update.code.visualstudio.com/#{version}/#{os}-#{arch}/stable"
   name "Microsoft Visual Studio Code"
