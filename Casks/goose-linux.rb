fa54361402be5477066131330d59faf3ae75d714
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-02-28 04:18:33 +0000

    goose-linux 1.26.1

--- Casks/goose-linux.rb
+++ Casks/goose-linux.rb
@@ -1,6 +1,6 @@
 cask "goose-linux" do
-  version "1.23.0"
-  sha256 "0e6bb0f2d86df89a97d545a160895b45afba126954a2746c2aeb3c86dd8f1679"
+  version "1.26.1"
+  sha256 "d81123a6d5fd630cf757cef620a9822ccb8e9acc1ccc74c5a4f4ad3661442d19"
 
   url "https://github.com/block/goose/releases/download/v#{version}/Goose-#{version}-1.x86_64.rpm",
       verified: "github.com/block/goose/"
